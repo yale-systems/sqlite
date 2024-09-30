4a5f454fd847b2315fd47c4ee761bf76213b350d
pako-23 <pasqualepolverino98@gmail.com>
2024-09-30 23:25:19 +0200

    fix typo

--- src/os_setup.h
+++ src/os_setup.h
@@ -41,7 +41,7 @@
 #    define SLQITE_OS_OTHER 1
 #    define SQLITE_OS_WIN 0
 #    define SQLITE_OS_UNIX 0
-#  if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || \
+#  elif defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || \
           defined(__MINGW32__) || defined(__BORLANDC__)
 #    define SQLITE_OS_WIN 1
 #    define SQLITE_OS_UNIX 0
